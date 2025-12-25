2266ac7158c1f42e8689d80240dcf71b1289e99f
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2025-12-26 00:00:32 +0530

    add invalid move message

--- constants.js
+++ constants.js
@@ -1,14 +1,15 @@
 export const WIN_MESSAGE = 'Congratulations! You Won The Game';
-export const LOSE_MESSAGE = 'Congruatulations! You Lost The Game';
+export const LOSE_MESSAGE = 'Congratulations! You Lost The Game';
 
 export const ROW_HEADER = '1 2 3 4 5 6 7';
 
 export const CELL_INPUT_MSG = 'Enter Row And Column (e.g., "4 4"):';
 export const CELL_INPUT_ERR =
-	'Invalid input. Enter two numbers between 1 and 7.';
+	'Invalid input. Enter two numbers inside the board.';
 
 export const DIR_INPUT_MSG = 'Direction (U, D, L, R):';
 export const DIR_INPUT_ERR = 'Invalid direction. Use U, D, L, or R.';
+export const MOVES_ERR = 'No moves available for this peg. Choose another.';
 
 export const DIRECTIONS = {
 	UP: { dr: -2, dc: 0 },
