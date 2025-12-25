b8077c708cd2979cddcf53d60a1434e42b50dae3
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2025-12-25 15:34:01 +0530

    improve UI functions

--- main.js
+++ main.js
@@ -1,75 +1,97 @@
-import { createBoard, DIRECTIONS, PEG, HOLE, INVALID } from './board.js';
+import { createBoard } from './board.js';
 import { createGame } from './game.js';
+import {
+	ROW_HEADER,
+	LOSE_MESSAGE,
+	WIN_MESSAGE,
+	CELL_INPUT_MSG,
+	CELL_INPUT_ERR,
+	DIR_INPUT_MSG,
+	DIR_INPUT_ERR,
+	PEG,
+	HOLE,
+	INVALID,
+	INPUT_MAP,
+} from './constants.js';
 
-const INPUT_MAP = {
-	U: DIRECTIONS.UP,
-	D: DIRECTIONS.DOWN,
-	L: DIRECTIONS.LEFT,
-	R: DIRECTIONS.RIGHT,
+const underline = (text) =>
+	text
+		.split('')
+		.map((_) => '-')
+		.join('');
+
+const parseCoords = (input) =>
+	input
+		.trim()
+		.split(' ')
+		.map((x) => parseInt(x) - 1);
+
+const parseDirection = (input) => INPUT_MAP[input.toUpperCase()];
+
+const isInside = (n) => !isNaN(n) && n >= 0 && n <= 6;
+const isValidCoord = (row, col) => isInside(row) && isInside(col);
+
+const endGame = (state) => {
+	const message = state === 'WON' ? WIN_MESSAGE : LOSE_MESSAGE;
+	console.log(message);
+};
+
+const displayAvailableMoves = (availableMoves) => {
+	console.log('Available Directions:', availableMoves.join(' '));
+};
+
+const getPegPosition = () => {
+	const input = prompt(CELL_INPUT_MSG);
+	if (!input) {
+		return getPegPosition();
+	}
+	const [row, col] = parseCoords(input);
+	if (!isValidCoord(row, col)) {
+		console.log(CELL_INPUT_ERR);
+		return getPegPosition();
+	}
+	return [row, col];
+};
+
+const getPegDirection = (availableMoves) => {
+	const direction = prompt(DIR_INPUT_MSG);
+	const validMoves = availableMoves.map((move) => move[0].toLowerCase());
+	if (!direction || !validMoves.includes(direction)) {
+		console.log(DIR_INPUT_ERR);
+		return getPegDirection(availableMoves);
+	}
+	return parseDirection(direction);
+};
+
+const formatRow = (row, rowNum) => {
+	const symbols = { [PEG]: 'O', [HOLE]: '.', [INVALID]: ' ' };
+	return [rowNum, row.map((cell) => symbols[cell]).join(' ')].join(' | ');
 };
 
 const displayBoard = (board) => {
-	const symbols = { [PEG]: 'O', [HOLE]: '.', [INVALID]: ' ' };
-
-	console.log('    1 2 3 4 5 6 7');
-	console.log('   ---------------');
+	console.log(ROW_HEADER.padStart(ROW_HEADER.length + 4, ' '));
+	console.log(underline(ROW_HEADER).padStart(ROW_HEADER.length + 4, ' '));
 
 	board.forEach((row, index) => {
-		const rowNum = index + 1;
-		const rowString = row.map((c) => symbols[c]).join(' ');
-		console.log(`${rowNum} | ${rowString}`);
+		console.log(formatRow(row, index + 1));
 	});
 	console.log('');
 };
 
-const askUntilValid = (question, parseFn, isValidFn, errorMsg) => {
-	const input = prompt(question);
-
-	if (!input) return askUntilValid(question, parseFn, isValidFn, errorMsg);
-
-	const result = parseFn(input);
-	if (!isValidFn(result)) {
-		console.log(errorMsg);
-		askUntilValid(question, parseFn, isValidFn, errorMsg);
-	}
-	return result;
-};
-
-const displayAvailableMoves = (board, row, col) => {
-	const availableMoves = board.availableMoves(row, col);
-	console.log('Available Directions:', availableMoves.join(''));
-};
-
-const endGame = (state) => {
-	state === 'WON'
-		? console.log('You Won the Game')
-		: console.log('You Lost the Game');
-};
-
-const main = () => {
+const initGame = () => {
 	const board = createBoard();
 	const game = createGame(board);
 
-	let message = '';
 	while (game.getStatus() === 'PLAYING') {
 		console.clear();
 		displayBoard(board.boardState());
-		if (message) console.log(`\n📢 ${message}\n`);
-		message = '';
 		const [row, col] = getPegPosition();
-
-		displayAvailableMoves(board, row, col);
-
-		const direction = getPegDirection();
-
-		const result = game.playMove(row, col, direction);
-
-		if (!result.success) {
-			message = '❌ Invalid Move! Try again.';
-		}
+		const availableMoves = board.availableMoves(row, col);
+		displayAvailableMoves(availableMoves);
+		const direction = getPegDirection(availableMoves);
+		game.playMove(row, col, direction);
 	}
-
 	endGame(game.getStatus());
 };
 
-main();
+initGame();
